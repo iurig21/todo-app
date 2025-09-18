a5876e6c368dc99f64636b20ae4c21ecf7563b4e
iurig21 <a32192@alunos.ipca.pt>
2025-09-18 17:42:36 +0100

    style: changed the placeholder to english

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ function SearchBar({OnSerchBarChange}){
 
     return (
         <div className="w-full">
-            <Input placeholder="Pesquise uma tarefa" onChange={(event) => OnSerchBarChange(event.target.value)}/>
+            <Input placeholder="Search for a task" onChange={(event) => OnSerchBarChange(event.target.value)}/>
         </div>
     )
 }
