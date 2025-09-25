5a7d12d31edd1622d8cf18780edeaa1ddc3616be
iurig21 <a32192@alunos.ipca.pt>
2025-09-25 19:14:22 +0100

    styles: updated padding

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,6 @@
 function Input(props){   
     return(
-        <input className="w-full bg-white p-2 rounded-sm shadow-xl"
+        <input className="w-full bg-white p-3 rounded-sm shadow-xl"
             {...props}
         />
     )
